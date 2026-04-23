d7ee09bfe1a7d7c32dbd42410475a65d714fbada
kono <94098626+ayatsuki-meowmeow@users.noreply.github.com>
2026-04-23 19:48:28 +0900

    scaffoldにするための修正 (#4)
    * improve schema
    
    * 生成先の変更
    
    * CRUD example

--- apps/web/orval.config.ts
+++ apps/web/orval.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "orval";
 
-// TODO: アーキテクチャを決めたら出力先を変更する
 export default defineConfig({
   api: {
     input: "../../openapi.yaml",
     output: {
       mode: "tags-split",
-      target: "src/generated/api.ts",
+      target: "src/generated",
       schemas: "src/generated/model",
       client: "react-query",
       clean: true,
+      override: {
+        mutator: {
+          path: "src/lib/fetcher.ts",
+          name: "fetcher",
+        },
+      },
     },
   },
 });
